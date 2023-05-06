6c629e021c8daabbc8c56d8986cf90b81b36b3ea
Arman7052 <99arman7052@gmail.com>
2023-05-07 00:36:08 +0600

    Client site to server Site

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit =event => {
+    event.preventDefault();
+    const form = event.target;
+    const name =form.name.value;
+    const email = form.email.value;
+    const user ={name, email };
+    console.log(user);
+    fetch('http://localhost:5000/users',{
+          method:'POSt',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('Inside post response ', data);
+    })
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  },[])
+  console.log(users);
+  return (
+    <>
+
+      <h1>User Management</h1>
+        <p>{users.length}</p>
+        <form onSubmit={handleSubmit} action="">
+          <input type="text" name="name" id="" />
+          <br />
+          <input type="email" name="email" id="" />
+          <br />
+          <input type="submit" value="Add user" />
+        </form>
+        <div>
+          {
+            users.map(user => <p key={user.id}> {user.id} : {user.name} : {user.email} </p>)
+          }
+        </div>
+    </>
+  )
+} 
+
+export default App
